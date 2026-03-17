6d9441ecfbc36075fbe1841f04f6e65d075d4453
Ishaan Shiv Kumar <ishaanshivkr@gmail.com>
2026-03-17 18:26:11 +0530

    Defined central system

---
+++ integration/code/system.py
@@ -0,0 +1,9 @@
+from models import CrewMember, Inventory, Race, Mission
+
+
+class StreetRaceSystem:
+    def __init__(self):
+        self.crew: dict[str, CrewMember] = {}
+        self.inventory = Inventory()
+        self.races: list[Race] = []
+        self.missions: list[Mission] = []
